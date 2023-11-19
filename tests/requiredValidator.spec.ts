d3c2b064bea1dc04e729429da6b43c5bda78ccf1
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-11-19 11:18:05 -0300

    test: :test_tube: add tests for requiredValidator function

---
+++ tests/requiredValidator.spec.ts
@@ -0,0 +1,30 @@
+import { Schema } from '../src/index';
+
+describe('Required Validator', () => {
+  it('should throw error if required field does not follow rules of a required value', async () => {
+    const schema = new Schema({ role: '' });
+    const wrapper = () => schema.validate('role').required();
+    expect(wrapper).toThrow();
+  });
+
+  it('should not throw error if required field follow rules of a required value', async () => {
+    const schema = new Schema({ role: 'admin' });
+    const wrapper = () => schema.validate('role').required();
+    expect(wrapper).not.toThrow();
+  });
+
+  it('should be able to set a custom error message', async () => {
+    const customErrorMessage = 'FIELD IS NOT CORRECT';
+    const schema = new Schema({ role: null });
+    const wrapper = () => schema.validate('role').required(customErrorMessage);
+    expect(wrapper).toThrow(customErrorMessage);
+  });
+
+  it('should be able to nest required validation function with other validations', async () => {
+    const schema = new Schema({ role: 'admin', nonRequired: null });
+    const wrapper = () => {
+      schema.validate('role').required().string();
+    };
+    expect(wrapper).not.toThrow();
+  });
+});
