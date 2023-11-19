4e3c68588ee84e81000fd65d4ca67b6b2c9b0454
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-11-19 10:58:24 -0300

    test: :test_tube: add tests for schema

--- tests/validator.spec.ts
+++ tests/validator.spec.ts
@@ -1,3 +1,5 @@
+import { ValidateError } from '../src/ValidateError';
+import { defaultErrors } from '../src/defaultErrors';
 import { Schema } from '../src/index';
 describe('String Validator', () => {
   it('should throw error if field value is not a string', async () => {
@@ -34,5 +36,36 @@ describe('Schema class', () => {
     //@ts-expect-error Expect years to not be keyof Schema values
     const wrapper = () => schema.validate('years').string();
     expect(wrapper).toThrow();
+  });
+
+  it('should be able to validate multiple fields at the same time with errors', async () => {
+    const expectedError = new ValidateError(defaultErrors.notString('years'));
+    const schema = new Schema({
+      role: 'admin',
+      years: 10,
+      name: 'Joao',
+      nickname: 'jao',
+    });
+    const wrapper = () => {
+      schema.validate('role').string();
+      schema.validate('years').string();
+      schema.validate('nickname').string();
+      schema.validate('nickname').string();
+    };
+    expect(wrapper).toThrow(expectedError);
+  });
+
+  it('should be able to validate multiple field at the same time with no errors', async () => {
+    const schema = new Schema({
+      role: 'admin',
+      name: 'Joao',
+      nickname: 'jao',
+    });
+    const wrapper = () => {
+      schema.validate('role').string();
+      schema.validate('nickname').string();
+      schema.validate('nickname').string();
+    };
+    expect(wrapper).not.toThrow();
   });
 });
