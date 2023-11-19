66518b5cb3a0ecb5e6ef91baff0c7aa95ef2d0ea
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-11-19 14:47:49 -0300

    feat: :sparkles: add custom error class

--- src/ValidateError.ts
+++ src/ValidateError.ts
@@ -1,1 +1,9 @@
-export class ValidateError extends Error {}
+export class ValidationError extends Error {
+  field: string;
+
+  constructor(message: string, field: string) {
+    super(message);
+    this.field = field;
+    this.name = 'ValidationError';
+  }
+}
