5e220d4a351699110edbb1028d3edf709749473d
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-11-19 15:14:15 -0300

    test: :fire: remove unused test

--- tests/stringValidator.spec.ts
+++ tests/stringValidator.spec.ts
@@ -16,12 +16,6 @@ describe('String Validator', () => {
     expect(wrapper).not.toThrow();
   });
 
-  it('should return which field has an error', async () => {
-    const schema = new Schema({ role: 1 });
-    const wrapper = () => schema.validate('role').string();
-    expect(wrapper).toThrow();
-  });
-
   it('should be able to set a custom error message', async () => {
     const customErrorMessage = 'FIELD IS NOT CORRECT';
     const schema = new Schema({ role: 1 });
