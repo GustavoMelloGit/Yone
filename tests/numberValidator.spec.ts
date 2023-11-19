1473da87df6acaab5c4b7f725f8fb944a815eee6
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-11-19 15:14:00 -0300

    test: :test_tube:
    add tests for numberValidator

---
+++ tests/numberValidator.spec.ts
@@ -0,0 +1,36 @@
+import { ValidationError } from '../src/ValidateError';
+import { defaultErrors } from '../src/defaultErrors';
+import { Schema } from '../src/index';
+
+describe('Number Validator', () => {
+  it('should throw error if field value is not a number', async () => {
+    const expectedError = new ValidationError(
+      defaultErrors.notNumber('role'),
+      'role'
+    );
+    const schema = new Schema({ role: 'admin' });
+    const wrapper = () => schema.validate('role').number();
+    expect(wrapper).toThrow(expectedError);
+  });
+
+  it('should not throw error if field value is a number', () => {
+    const schema = new Schema({ years: 4 });
+
+    const wrapper = () => schema.validate('years').number();
+
+    expect(wrapper).not.toThrow();
+  });
+
+  it('should be able to set a custom error message', async () => {
+    const customErrorMessage = 'FIELD IS NOT CORRECT';
+    const schema = new Schema({ role: 1 });
+    const wrapper = () => schema.validate('role').number(customErrorMessage);
+    expect(wrapper).toThrow(customErrorMessage);
+  });
+
+  it('should throw default error message if no custom message is given', async () => {
+    const schema = new Schema({ role: 10 });
+    const wrapper = () => schema.validate('role').number();
+    expect(wrapper).toThrow(defaultErrors.notNumber('role'));
+  });
+});
